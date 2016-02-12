a6102bd01cf28d9d346eee8321e8ada4459abf50
Mathias Roslund <mathias.roslund@amidog.se>
2016-02-12 11:05:28 +0100

    implemented lwr

--- mips/lwl_lwr.c
+++ mips/lwl_lwr.c
@@ -13,7 +13,7 @@ static const c8 data_be[5][12] =
   { '0', '1', '2', '3', '4', '5', '6', '7', 'I', 'J', 'K', 'L' }
 };
 
-static const c8 reg_be[4][4] =
+static const c8 reg_lwl_be[4][4] =
 {
   { 'I', 'J', 'K', 'L' },
   { 'J', 'K', 'L', 'h' },
@@ -25,7 +25,26 @@ static void lwl_be(s32 vAddr)
 {
   c8 reg[4] = { 'e', 'f', 'g', 'h' };
   s32 i, j;
-  for (j = 0, i = vAddr; i < 4; j++, i++)
+  for (j = 0, i = vAddr; i <= 3; j++, i++)
+  {
+    reg[j] = data_be[0][4 + i];
+  }
+  printf("vAddr %d: %c %c %c %c\n", vAddr, reg[0], reg[1], reg[2], reg[3]);
+}
+
+static const c8 reg_lwr_be[4][4] =
+{
+  { 'e', 'f', 'g', 'I' },
+  { 'e', 'f', 'I', 'J' },
+  { 'e', 'I', 'J', 'K' },
+  { 'I', 'J', 'K', 'L' }
+};
+
+static void lwr_be(s32 vAddr)
+{
+  c8 reg[4] = { 'e', 'f', 'g', 'h' };
+  s32 i, j;
+  for (j = (3 - vAddr), i = 0; i <= vAddr; j++, i++)
   {
     reg[j] = data_be[0][4 + i];
   }
@@ -41,7 +60,7 @@ static const c8 data_le[5][12] =
   { '0', '1', '2', '3', '4', '5', '6', '7', 'L', 'K', 'J', 'I' }
 };
 
-static const c8 reg_le[4][4] =
+static const c8 reg_lwl_le[4][4] =
 {
   { 'L', 'f', 'g', 'h' },
   { 'K', 'L', 'g', 'h' },
@@ -60,19 +79,50 @@ static void lwl_le(s32 vAddr)
   printf("vAddr %d: %c %c %c %c\n", vAddr, reg[0], reg[1], reg[2], reg[3]);
 }
 
+static const c8 reg_lwr_le[4][4] =
+{
+  { 'I', 'J', 'K', 'L' },
+  { 'e', 'I', 'J', 'K' },
+  { 'e', 'f', 'I', 'J' },
+  { 'e', 'f', 'g', 'I' }
+};
+
+static void lwr_le(s32 vAddr)
+{
+  c8 reg[4] = { 'e', 'f', 'g', 'h' };
+  s32 i, j;
+  for (j = vAddr, i = 3; i >= vAddr; j++, i--)
+  {
+    reg[j] = data_le[0][4 + i];
+  }
+  printf("vAddr %d: %c %c %c %c\n", vAddr, reg[0], reg[1], reg[2], reg[3]);
+}
+
 int main(void)
 {
-  printf("le\n");
+  printf("lwl le\n");
   lwl_le(0);
   lwl_le(1);
   lwl_le(2);
   lwl_le(3);
 
-  printf("be\n");
+  printf("lwl be\n");
   lwl_be(0);
   lwl_be(1);
   lwl_be(2);
   lwl_be(3);
+
+  printf("lwr le\n");
+  lwr_le(0);
+  lwr_le(1);
+  lwr_le(2);
+  lwr_le(3);
+
+  printf("lwr be\n");
+  lwr_be(0);
+  lwr_be(1);
+  lwr_be(2);
+  lwr_be(3);
 
   return 0;
 }
