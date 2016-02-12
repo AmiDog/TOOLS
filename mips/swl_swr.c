3facfdeab662e35a6e6e890a78b6661a790f4777
Mathias Roslund <mathias.roslund@amidog.se>
2016-02-12 11:38:19 +0100

    store implemented

--- mips/swl_swr.c
+++ mips/swl_swr.c
@@ -11,40 +11,40 @@ static const c8 reg_be[4] =
 
 static const c8 mem_swl_be[4][4] =
 {
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' }
+  { 'E', 'F', 'G', 'H' },
+  { 'i', 'E', 'F', 'G' },
+  { 'i', 'j', 'E', 'F' },
+  { 'i', 'j', 'k', 'E' }
 };
 
 static void swl_be(s32 vAddr)
 {
-  c8 reg[4] = { 'e', 'f', 'g', 'h' };
+  c8 mem[4] = { 'i', 'j', 'k', 'l' };
   s32 i, j;
-  for (j = 0, i = vAddr; i <= 3; j++, i++)
+  for (j = vAddr, i = 0; i <= (3 - vAddr); j++, i++)
   {
-    reg[j] = reg_be[i];
+    mem[j] = reg_be[i];
   }
-  printf("vAddr %d: %c %c %c %c\n", vAddr, reg[0], reg[1], reg[2], reg[3]);
+  printf("vAddr %d: %c %c %c %c\n", vAddr, mem[0], mem[1], mem[2], mem[3]);
 }
 
 static const c8 mem_swr_be[4][4] =
 {
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' }
+  { 'H', 'j', 'k', 'l' },
+  { 'G', 'H', 'k', 'l' },
+  { 'F', 'G', 'H', 'l' },
+  { 'E', 'F', 'G', 'H' }
 };
 
 static void swr_be(s32 vAddr)
 {
-  c8 reg[4] = { 'e', 'f', 'g', 'h' };
+  c8 mem[4] = { 'i', 'j', 'k', 'l' };
   s32 i, j;
-  for (j = (3 - vAddr), i = 0; i <= vAddr; j++, i++)
+  for (j = 0, i = (3 - vAddr); i <= 3; j++, i++)
   {
-    reg[j] = reg_be[i];
+    mem[j] = reg_be[i];
   }
-  printf("vAddr %d: %c %c %c %c\n", vAddr, reg[0], reg[1], reg[2], reg[3]);
+  printf("vAddr %d: %c %c %c %c\n", vAddr, mem[0], mem[1], mem[2], mem[3]);
 }
 
 static const c8 reg_le[4] =
@@ -54,40 +54,40 @@ static const c8 reg_le[4] =
 
 static const c8 mem_swl_le[4][4] =
 {
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' }
+  { 'i', 'j', 'k', 'E' },
+  { 'i', 'j', 'E', 'F' },
+  { 'i', 'E', 'F', 'G' },
+  { 'E', 'F', 'G', 'H' }
 };
 
 static void swl_le(s32 vAddr)
 {
-  c8 reg[4] = { 'e', 'f', 'g', 'h' };
+  c8 mem[4] = { 'i', 'j', 'k', 'l' };
   s32 i, j;
-  for (j = 0, i = vAddr; i >= 0; j++, i--)
+  for (j = (3 - vAddr), i = 0; i <= vAddr; j++, i++)
   {
-    reg[j] = reg_le[i];
+    mem[j] = reg_be[i];
   }
-  printf("vAddr %d: %c %c %c %c\n", vAddr, reg[0], reg[1], reg[2], reg[3]);
+  printf("vAddr %d: %c %c %c %c\n", vAddr, mem[0], mem[1], mem[2], mem[3]);
 }
 
 static const c8 mem_swr_le[4][4] =
 {
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' },
-  { 'i', 'j', 'k', 'l' }
+  { 'E', 'F', 'G', 'H' },
+  { 'F', 'G', 'H', 'l' },
+  { 'G', 'H', 'k', 'l' },
+  { 'H', 'j', 'k', 'l' }
 };
 
 static void swr_le(s32 vAddr)
 {
-  c8 reg[4] = { 'e', 'f', 'g', 'h' };
+  c8 mem[4] = { 'i', 'j', 'k', 'l' };
   s32 i, j;
-  for (j = vAddr, i = 3; i >= vAddr; j++, i--)
+  for (j = 0, i = vAddr; i <= 3; j++, i++)
   {
-    reg[j] = reg_le[i];
+    mem[j] = reg_be[i];
   }
-  printf("vAddr %d: %c %c %c %c\n", vAddr, reg[0], reg[1], reg[2], reg[3]);
+  printf("vAddr %d: %c %c %c %c\n", vAddr, mem[0], mem[1], mem[2], mem[3]);
 }
 
 void test_swl_swr(void)
