a309f864bfd9490023bdc993d5b09ef174939d13
Mathias Roslund <mathias.roslund@amidog.se>
2016-02-17 13:53:39 +0100

    bugfixed, optimized and with more tests

--- mips/lwl_lwr.c
+++ mips/lwl_lwr.c
@@ -14,6 +14,16 @@ static const c8 mem_le[4] =
   'L', 'K', 'J', 'I'
 };
 
+static const c8 reg_be[4] =
+{
+  'e', 'f', 'g', 'h'
+};
+
+static const c8 reg_le[4] =
+{
+  'e', 'f', 'g', 'h'
+};
+
 /* LWL - EffAddr is the address of the most-significant of four consecutive
  * bytes forming a word in memory starting at an arbitrary byte boundary.
  */
@@ -94,19 +104,19 @@ static void lwr_le(c8 *reg, s32 vAddr, const c8 *mem)
 
 static const c8 data_be[5][12] =
 {
-  { '0', '1', '2', '3', 'I', 'J', 'K', 'L', '4', '5', '6', '7' },
-  { '0', '1', '2', '3', '4', 'I', 'J', 'K', 'L', '5', '6', '7' },
-  { '0', '1', '2', '3', '4', '5', 'I', 'J', 'K', 'L', '6', '7' },
-  { '0', '1', '2', '3', '4', '5', '6', 'I', 'J', 'K', 'L', '7' },
+  { '0', '1', '2', '3', 'I', 'J', 'K', 'L', '8', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', 'I', 'J', 'K', 'L', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', 'I', 'J', 'K', 'L', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', '6', 'I', 'J', 'K', 'L', 'b' },
   { '0', '1', '2', '3', '4', '5', '6', '7', 'I', 'J', 'K', 'L' }
 };
 
 static const c8 data_le[5][12] =
 {
-  { '0', '1', '2', '3', 'L', 'K', 'J', 'I', '4', '5', '6', '7' },
-  { '0', '1', '2', '3', '4', 'L', 'K', 'J', 'I', '5', '6', '7' },
-  { '0', '1', '2', '3', '4', '5', 'L', 'K', 'J', 'I', '6', '7' },
-  { '0', '1', '2', '3', '4', '5', '6', 'L', 'K', 'J', 'I', '7' },
+  { '0', '1', '2', '3', 'L', 'K', 'J', 'I', '8', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', 'L', 'K', 'J', 'I', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', 'L', 'K', 'J', 'I', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', '6', 'L', 'K', 'J', 'I', 'b' },
   { '0', '1', '2', '3', '4', '5', '6', '7', 'L', 'K', 'J', 'I' }
 };
 
@@ -126,40 +136,40 @@ static void lwr_lwl_le(c8 *reg, s32 EffAddr, const c8 *mem)
 
 typedef void (*op_t)(c8 *reg, s32 vAddr, const c8 *mem);
 
-static void do_op(const c8 *name, op_t op, const c8 *wanted, s32 vAddr, const c8 *mem)
+static void do_op(const c8 *name, op_t op, const c8 *wanted, s32 vAddr, const c8 *mem, const c8 *reg)
 {
-  c8 reg[4] = { 'e', 'f', 'g', 'h' };
+  c8 r[4] = { reg[0], reg[1], reg[2], reg[3] };
   s32 i;
-  op(reg, vAddr, mem);
-  printf("%s vAddr %d: %c %c %c %c\n", name, vAddr, reg[0], reg[1], reg[2], reg[3]);
+  op(r, vAddr, mem);
+  printf("%s vAddr %d: %c %c %c %c\n", name, vAddr, r[0], r[1], r[2], r[3]);
   for (i = 0; i < 4; i++)
   {
-    if (reg[i] != wanted[i])
+    if (r[i] != wanted[i])
     {
       printf("error @ %d\n", i);
     }
   }
 }
 
-#define DO_OP(_o, _e)                                                         \
-  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[0], 0, mem_ ## _e); \
-  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[1], 1, mem_ ## _e); \
-  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[2], 2, mem_ ## _e); \
-  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[3], 3, mem_ ## _e); \
+#define DO_OP(_o, _e)                                                                     \
+  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[0], 0, mem_ ## _e, reg_ ## _e); \
+  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[1], 1, mem_ ## _e, reg_ ## _e); \
+  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[2], 2, mem_ ## _e, reg_ ## _e); \
+  do_op(#_o "_" #_e, _o ## _ ## _e, reg_ ## _o ## _ ## _e[3], 3, mem_ ## _e, reg_ ## _e); \
   printf("\n");
 
 typedef void (*op_rl_t)(c8 *reg, s32 EffAddr, const c8 *mem);
 
-static void do_rl_op(const c8 *name, op_rl_t op, s32 EffAddr, const c8 *mem)
+static void do_rl_op(const c8 *name, op_rl_t op, s32 EffAddr, const c8 *mem, const c8 *reg)
 {
-  c8 reg[4] = { 'e', 'f', 'g', 'h' };
-  c8 wanted[4] = { 'I', 'J', 'K', 'L' };
+  const c8 wanted[4] = { 'I', 'J', 'K', 'L' };
+  c8 r[4] = { reg[0], reg[1], reg[2], reg[3] };
   s32 i;
-  op(reg, EffAddr, mem);
-  printf("%s EffAddr %d: %c %c %c %c\n", name, EffAddr, reg[0], reg[1], reg[2], reg[3]);
+  op(r, EffAddr, mem);
+  printf("%s EffAddr %d: %c %c %c %c\n", name, EffAddr, r[0], r[1], r[2], r[3]);
   for (i = 0; i < 4; i++)
   {
-    if (reg[i] != wanted[i])
+    if (r[i] != wanted[i])
     {
       printf("error @ %d\n", i);
     }
@@ -177,8 +187,8 @@ void test_lwl_lwr(void)
 
   for (i = 0; i < 5; i++)
   {
-    do_rl_op("lwr_lwl_be", lwr_lwl_be, 4 + i, data_be[i]);
-    do_rl_op("lwr_lwl_le", lwr_lwl_le, 4 + i, data_le[i]);
+    do_rl_op("lwr_lwl_be", lwr_lwl_be, 4 + i, data_be[i], reg_be);
+    do_rl_op("lwr_lwl_le", lwr_lwl_le, 4 + i, data_le[i], reg_le);
   }
   printf("\n");
 }
--- mips/swl_swr.c
+++ mips/swl_swr.c
@@ -4,6 +4,16 @@
 
 #include "main.h"
 
+static const c8 mem_be[4] =
+{
+  'i', 'j', 'k', 'l'
+};
+
+static const c8 mem_le[4] =
+{
+  'l', 'k', 'j', 'i'
+};
+
 static const c8 reg_be[4] =
 {
   'E', 'F', 'G', 'H',
@@ -29,26 +39,26 @@ static const c8 mem_swl_be[4][4] =
 static void swl_be(c8 *mem, s32 vAddr, const c8 *reg)
 {
   s32 i, j;
-  for (j = vAddr, i = 0; i <= (3 - vAddr); j++, i++)
+  for (j = 0, i = vAddr; i <= (vAddr | 3); j++, i++)
   {
-    mem[j] = reg[i];
+    mem[i] = reg[j];
   }
 }
 
 static const c8 mem_swl_le[4][4] =
 {
-  { 'i', 'j', 'k', 'E' },
-  { 'i', 'j', 'E', 'F' },
-  { 'i', 'E', 'F', 'G' },
-  { 'E', 'F', 'G', 'H' }
+  { 'E', 'k', 'j', 'i' },
+  { 'F', 'E', 'j', 'i' },
+  { 'G', 'F', 'E', 'i' },
+  { 'H', 'G', 'F', 'E' }
 };
 
 static void swl_le(c8 *mem, s32 vAddr, const c8 *reg)
 {
   s32 i, j;
-  for (j = (3 - vAddr), i = 0; i <= vAddr; j++, i++)
+  for (j = 0, i = vAddr; i >= (vAddr & ~3); j++, i--)
   {
-    mem[j] = reg[i];
+    mem[i] = reg[j];
   }
 }
 
@@ -67,40 +77,97 @@ static const c8 mem_swr_be[4][4] =
 static void swr_be(c8 *mem, s32 vAddr, const c8 *reg)
 {
   s32 i, j;
-  for (j = 0, i = (3 - vAddr); i <= 3; j++, i++)
+  for (j = 3, i = vAddr; i >= (vAddr & ~3); j--, i--)
   {
-    mem[j] = reg[i];
+    mem[i] = reg[j];
   }
 }
 
 static const c8 mem_swr_le[4][4] =
 {
-  { 'E', 'F', 'G', 'H' },
-  { 'F', 'G', 'H', 'l' },
-  { 'G', 'H', 'k', 'l' },
-  { 'H', 'j', 'k', 'l' }
+  { 'H', 'G', 'F', 'E' },
+  { 'l', 'H', 'G', 'F' },
+  { 'l', 'k', 'H', 'G' },
+  { 'l', 'k', 'j', 'H' }
 };
 
 static void swr_le(c8 *mem, s32 vAddr, const c8 *reg)
 {
   s32 i, j;
-  for (j = 0, i = vAddr; i <= 3; j++, i++)
+  for (j = 3, i = vAddr; i <= (vAddr | 3); j--, i++)
   {
-    mem[j] = reg[i];
+    mem[i] = reg[j];
   }
+}
+
+/* SWR + SWL */
+
+static const c8 data_be[5][12] =
+{
+  { '0', '1', '2', '3', 'E', 'F', 'G', 'H', '8', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', 'E', 'F', 'G', 'H', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', 'E', 'F', 'G', 'H', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', '6', 'E', 'F', 'G', 'H', 'b' },
+  { '0', '1', '2', '3', '4', '5', '6', '7', 'E', 'F', 'G', 'H' }
+};
+
+static const c8 data_le[5][12] =
+{
+  { '0', '1', '2', '3', 'H', 'G', 'F', 'E', '8', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', 'H', 'G', 'F', 'E', '9', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', 'H', 'G', 'F', 'E', 'a', 'b' },
+  { '0', '1', '2', '3', '4', '5', '6', 'H', 'G', 'F', 'E', 'b' },
+  { '0', '1', '2', '3', '4', '5', '6', '7', 'H', 'G', 'F', 'E' }
+};
+
+static void swr_swl_be(c8 *reg, s32 EffAddr, const c8 *mem)
+{
+  swr_be(reg, 3 + EffAddr, mem);
+  swl_be(reg, 0 + EffAddr, mem);
+}
+
+static void swr_swl_le(c8 *reg, s32 EffAddr, const c8 *mem)
+{
+  swr_le(reg, 0 + EffAddr, mem);
+  swl_le(reg, 3 + EffAddr, mem);
 }
 
 /* TEST */
 
 typedef void (*op_t)(c8 *mem, s32 vAddr, const c8 *reg);
 
-static void do_op(const c8 *name, op_t op, const c8 *wanted, s32 vAddr, const c8 *reg)
+static void do_op(const c8 *name, op_t op, const c8 *wanted, s32 vAddr, const c8 *reg, const c8 *mem)
 {
-  c8 mem[4] = { 'i', 'j', 'k', 'l' };
+  c8 m[4] = { mem[0], mem[1], mem[2], mem[3] };
   s32 i;
-  op(mem, vAddr, reg);
-  printf("%s vAddr %d: %c %c %c %c\n", name, vAddr, mem[0], mem[1], mem[2], mem[3]);
+  op(m, vAddr, reg);
+  printf("%s vAddr %d: %c %c %c %c\n", name, vAddr, m[0], m[1], m[2], m[3]);
   for (i = 0; i < 4; i++)
+  {
+    if (m[i] != wanted[i])
+    {
+      printf("error @ %d\n", i);
+    }
+  }
+}
+
+#define DO_OP(_o, _e)                                                                     \
+  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[0], 0, reg_ ## _e, mem_ ## _e); \
+  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[1], 1, reg_ ## _e, mem_ ## _e); \
+  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[2], 2, reg_ ## _e, mem_ ## _e); \
+  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[3], 3, reg_ ## _e, mem_ ## _e); \
+  printf("\n");
+
+typedef void (*op_rl_t)(c8 *reg, s32 EffAddr, const c8 *mem);
+
+static void do_rl_op(const c8 *name, op_rl_t op, s32 EffAddr, const c8 *wanted)
+{
+  const c8 reg[4] = { 'E', 'F', 'G', 'H' };
+  c8 mem[12] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b' };
+  s32 i;
+  op(mem, EffAddr, reg);
+  printf("%s EffAddr %d: %c %c %c %c %c %c %c %c %c %c %c %c\n", name, EffAddr, mem[0], mem[1], mem[2], mem[3], mem[4], mem[5], mem[6], mem[7], mem[8], mem[9], mem[10], mem[11]);
+  for (i = 0; i < 12; i++)
   {
     if (mem[i] != wanted[i])
     {
@@ -109,17 +176,19 @@ static void do_op(const c8 *name, op_t op, const c8 *wanted, s32 vAddr, const c8
   }
 }
 
-#define DO_OP(_o, _e)                                                         \
-  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[0], 0, reg_ ## _e); \
-  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[1], 1, reg_ ## _e); \
-  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[2], 2, reg_ ## _e); \
-  do_op(#_o "_" #_e, _o ## _ ## _e, mem_ ## _o ## _ ## _e[3], 3, reg_ ## _e); \
-  printf("\n");
-
 void test_swl_swr(void)
 {
+  s32 i;
+
   DO_OP(swl, le);
   DO_OP(swl, be);
   DO_OP(swr, le);
   DO_OP(swr, be);
+
+  for (i = 0; i < 5; i++)
+  {
+    do_rl_op("swr_swl_be", swr_swl_be, 4 + i, data_be[i]);
+    do_rl_op("swr_swl_le", swr_swl_le, 4 + i, data_le[i]);
+  }
+  printf("\n");
 }
